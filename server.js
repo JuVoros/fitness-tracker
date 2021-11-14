85f058a6b1a4b6889bcba9bbe3a6ae9c8f524907
JuVoros <Julianvoros@hotmail.com>
2021-11-14 13:24:18 -0800

    added starter code and other  base files

---
+++ server.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+
+const PORT = process.env.PORT || 8080;
+
+// Add the other variables here
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongod://localhost/fitnessTracker", {useNewUrlParser: true });
+
+
+// Add app get later
+
+app.listen(PORT, () => {
+    console.log(`App sprinting on port ${PORT}!`);
+});
+
+
+
